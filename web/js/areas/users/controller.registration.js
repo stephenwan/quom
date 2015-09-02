7edd423a5971f17018f5de18bcee8902b675a1e1
stephenwan <stephenwyy@gmail.com>
2015-09-03 01:04:00 +0800

    Basic registration UI finished.

--- web/js/areas/users/controller.registration.js
+++ web/js/areas/users/controller.registration.js
@@ -4,11 +4,11 @@
 
     angular.module('quom.users')
            .controller('UserRegistrationCtrl', [             
-             '$scope', '$element', '$timeout',
+             '$scope', '$element', '$timeout', '$location',
              ctrlFn]);
 
 
-    function ctrlFn($scope, $element, $timeout) {
+    function ctrlFn($scope, $element, $timeout, $location) {
         $scope.user = {
             email: '',
             firstName: '',
@@ -20,6 +20,16 @@
         
         $scope.nextStep = function() {
             $scope.stepIndex += 1;
+        };
+
+        $scope.submit = function() {
+            console.log('submit', $scope.user);
+        };
+
+        $scope.cancel = function() {
+            $scope.user = {};
+            $location.path('/login').replace();
+            
         };
     }
 
